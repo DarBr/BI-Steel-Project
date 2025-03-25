7d95ee2f3e2a9ffa701bc1de1c4026e0513fa5a1
alieniig <s_lienig22@hwr-berlin.de>
2025-03-25 13:15:48 +0100

    path

--- machine_learning/test.py
+++ machine_learning/test.py
@@ -1,8 +1,8 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Verzeichnis des aktuellen Skripts
-data_path = os.path.join(BASE_DIR, "/energy_prices_data.csv")
-model_path = os.path.join(BASE_DIR, "/energy_price_model.h5")
+data_path = os.path.join(BASE_DIR, "energy_prices_data.csv")  # Kein führender /
+model_path = os.path.join(BASE_DIR, "energy_price_model.h5")  # Kein führender /
 
 print(data_path)
 print(model_path)
